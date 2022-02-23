94b24317ea36732d0b34f214627d3e6b86e97765
vugar <28726265+vugar005@users.noreply.github.com>
2022-02-23 23:34:23 +0100

    Feature/add likes app (#3)
    * feature/add-likes-app
    
    Co-authored-by: Vugar_Abdullayev <vugar_abdullayev@epam.com>

---
+++ likes-app/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { UIStoreService } from './core/services/ui-store/ui-store.service';
+
+@Component({
+  selector: 'likes-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  @Input() likedVideoList?: string[];
+
+  constructor(private router: Router, private uiStore: UIStoreService, private title: Title) {}
+
+  public ngOnInit(): void {
+    this.connectRouter();
+    this.setMetaTags();
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    const likedVideoListChange = changes && changes['likedVideoList'];
+    if (likedVideoListChange) {
+      const likedVideoListData = likedVideoListChange.currentValue;
+      const data = likedVideoListData ? likedVideoListData.split(',') : [];
+      this.uiStore.setLikedVideosList({ videoIds: data });
+    }
+  }
+
+  private connectRouter(): void {
+    const url = `${location.pathname.substr(1)}${location.search}`;
+    this.router.navigateByUrl(url);
+    window.addEventListener('popstate', () => {
+      this.router.navigateByUrl(url);
+    });
+  }
+
+  private setMetaTags(): void {
+    this.title.setTitle(`Liked Videos`);
+  }
+}
