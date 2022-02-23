9756e57b7b1601716c1d55ff51d784a3ad713159
Vugar_Abdullayev <vugar_abdullayev@epam.com>
2022-02-23 23:33:27 +0100

    feature/add-likes-app

--- likes-app/src/app/app.module.ts
+++ likes-app/src/app/app.module.ts
@@ -1,16 +1,64 @@
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { DoBootstrap, Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+
 import { BrowserModule } from '@angular/platform-browser';
+import { MatDividerModule } from '@angular/material/divider';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import {
+  APP_CONFIG,
+  VideoThumbnailLoaderModule,
+  VideoThumbnailModule,
+  YoutubeServiceV2,
+  YOUTUBE_SERVICE,
+} from '@youtube/common-ui';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
+import { HomeComponent } from './home/home.component';
+import { ROOT_REDUCERS } from './reducers';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    VideoThumbnailModule,
+    VideoThumbnailLoaderModule,
+    MatDividerModule,
+    StoreModule.forRoot(ROOT_REDUCERS, {
+      runtimeChecks: {
+        // strictStateImmutability and strictActionImmutability are enabled by default
+        strictStateSerializability: true,
+        strictActionSerializability: true,
+        strictActionWithinNgZone: true,
+        strictActionTypeUniqueness: true,
+      },
+    }),
+    !environment.production
+      ? StoreDevtoolsModule.instrument({
+          name: 'Youtube Likes App Store',
+        })
+      : [],
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: YOUTUBE_SERVICE,
+      useClass: YoutubeServiceV2,
+    },
+    { provide: APP_CONFIG, useValue: environment },
+  ],
+  bootstrap: [],
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap {
+  constructor(private injector: Injector) {}
+
+  public ngDoBootstrap(): void {
+    const ce = createCustomElement(AppComponent, { injector: this.injector });
+    customElements.define('likes-app-element', ce);
+    // <watch-app-element></watch-app-element>
+  }
+}
