089c6478679c7f4e5d6efdaed0754225f5d3a053
Vugar_Abdullayev <vugar_abdullayev@epam.com>
2022-02-25 14:53:50 +0100

    add eslint and prettier

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,19 +3,20 @@
  * This is only a minimal backend to get started.
  */
 
- import { Logger } from '@nestjs/common';
- import { NestFactory } from '@nestjs/core';
- import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  app.enableCors();
+  const port = process.env.PORT || 3333;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
+}
 
- async function bootstrap() {
-   const app = await NestFactory.create(AppModule);
-   const globalPrefix = 'api';
-   app.setGlobalPrefix(globalPrefix);
-   app.enableCors();
-   const port = process.env.PORT || 3333;
-   await app.listen(port);
-   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
- }
-
- bootstrap();
+bootstrap();
