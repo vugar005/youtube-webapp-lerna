251c3212fcbd8cdd21fdaa06c487d3ff2ac7686d
Vugar_Abdullayev <vugar_abdullayev@epam.com>
2022-02-25 14:03:39 +0100

    feature/add-lerna-commands

---
+++ scripts/prepare-common-ui.js
@@ -0,0 +1,28 @@
+const { rimrafPromise, execPromise } = require('./utils');
+const fs = require('fs-extra');
+
+const uiDir = 'common-ui';
+const apps = ['shell', 'watch-app', 'likes-app', 'history-app'];
+
+async function  buildAndCopy() {
+    console.log('Building Common UI ...');
+    await rimrafPromise(`${uiDir}/dist`);
+    execPromise(`cd ${uiDir} && ng build ui`);
+    await copyToApps();
+}
+
+async function copyToApps() {
+    for (const appKey in apps) {
+        const appName = apps[appKey];
+        await rimrafPromise(`${appName}/dist`);
+        await rimrafPromise(`${appName}/.angular`);
+        try {
+            await fs.copySync(`./${uiDir}/dist`, `${appName}/node_modules`, { overwrite: true });
+            console.log(`Copied common-ui to ${appName}.`)
+        } catch (er) {
+            console.log(`er`);
+        }
+    }
+}
+
+buildAndCopy();
