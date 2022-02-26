f7a905d054df61cb53326fb0abe87cbe5fb217d5
Vugar <vugar.005.va@gmail.com>
2022-02-26 01:21:45 +0100

    feature/remember-mini-video-settings (#19)
    Co-authored-by: Vugar_Abdullayev <vugar_abdullayev@epam.com>

--- shell/src/app/core/services/video-store/video-store.service.ts
+++ shell/src/app/core/services/video-store/video-store.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { MiniVideoPayload } from '@youtube/common-ui';
+import { MiniVideoPayload, WebApiService, LocalStorageEnum } from '@youtube/common-ui';
 import { Observable } from 'rxjs';
 import { selectIsMiniPlayerMode, selectMiniPlayerVideo, selectVideoSearchQuery } from '../../../reducers';
 import { VideoActions } from '../../actions';
 
 @Injectable({ providedIn: 'root' })
 export class VideoStoreService {
-  constructor(private store: Store) {}
+  constructor(private store: Store, private webApiService: WebApiService) {}
 
   public setSearchQuery(query: string): void {
     this.store.dispatch(VideoActions.setVideoSearchQuery({ payload: query }));
@@ -17,8 +17,8 @@ export class VideoStoreService {
     return this.store.select(selectVideoSearchQuery);
   }
 
-  public setIsMiniPlayerMode(isEnabled: boolean): void {
-    this.store.dispatch(VideoActions.setIsMiniPlayerMode({ payload: isEnabled }));
+  public setIsMiniPlayerMode(enable: boolean): void {
+    this.store.dispatch(VideoActions.setIsMiniPlayerMode({ payload: enable }));
   }
 
   public selectIsMiniPlayerMode(): Observable<boolean> {
@@ -27,9 +27,19 @@ export class VideoStoreService {
 
   public setMiniPlayerVideo(payload: MiniVideoPayload): void {
     this.store.dispatch(VideoActions.setMiniPlayerVideo({ payload }));
+    this.saveMiniVideoSettings(payload);
   }
 
   public selectMiniPlayerVideo(): Observable<MiniVideoPayload> {
     return this.store.select(selectMiniPlayerVideo);
+  }
+
+  // save mini video settings so on page reload to show it again
+  private saveMiniVideoSettings(videoPayload: MiniVideoPayload | undefined): void {
+    if (videoPayload?.videoId && videoPayload?.startSeconds) {
+      this.webApiService.localStorage.setItem(LocalStorageEnum.MINI_WIDEO_SETTINGS, JSON.stringify(videoPayload));
+    } else {
+      this.webApiService.localStorage.removeItem(LocalStorageEnum.MINI_WIDEO_SETTINGS);
+    }
   }
 }
